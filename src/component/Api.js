c4a2e09be3e19685ace5c4e39dcc30230bc385e5
Pritam <pritamdebnath297@gmail.com>
2021-07-07 21:07:25 +0530

    :+1:  random images are successfully rendered

--- src/component/Api.js
+++ src/component/Api.js
@@ -2,20 +2,42 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 
-function Api() {
+const Api = () => {
+    
+    function GetImages() {
+        const count = 1;
+        const [state, setState] = useState([]);
+        useEffect(() => {
+            axios
+                .get("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=e74f279994089eba87d7030954569145&per_page=10&format=json&nojsoncallback=1")
+                .then((data)=>{
+                    setState(data.data.photos.photo)
+                })    
+        }, [count])
+        
+        return (
+                state
+        )
+    }
 
-    const [state, setState] = useState([]);
-    axios
-        .get("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=e74f279994089eba87d7030954569145&per_page=10&format=json&nojsoncallback=1")
-        .then((data)=>{
-            console.log(data.data.photos.photo)
-        })
-        console.log("hi")
-    return (
+    function getImageUrl(farm, server, id, secret) {
+        return `https://live.staticflickr.com/${server}/${id}_${secret}_q.jpg`;
+    }
+
+    const data = GetImages();
+    
+    return(
         <div>
-            hello everyone
+            {data.map((img)=>{
+                const { farm, server, id, secret } = img;
+                return(
+                    <img alt="" src={getImageUrl(farm, server, id, secret)} width="300px" />
+                )
+                
+            })}
         </div>
     )
+    
 }
 
 export default Api
