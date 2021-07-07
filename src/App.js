36c4e5897c839a7cc1ecf5a46a8ddc8680213034
Pritam <pritamdebnath297@gmail.com>
2021-07-07 22:13:49 +0530

    feat: added input
    search component is created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Api from './component/Api';
+import Search from './component/Search';
+import { useState } from 'react';
 
 
 
 function App() {
+
+  const [query, setQuery] = useState();
+  //console.log(query);
+  const search = () => {
+    console.log("button clicked")
+    //console.log(query)
+  }
+
   return (
     <div className="App">
+    <div>
+      <input type="text" onChange={(event) => setQuery(event.target.value)}/>
+      <button onClick={search}>Search</button>
+      {console.log(query)}
+    </div>
       <Api/>
     </div>
   );
--- src/component/Search.js
+++ src/component/Search.js
@@ -1,19 +1,17 @@
-import React, {useState, useEffect} from 'react'
+import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 
+const Search = () => {
 
-const Api = () => {
-    
-    function GetImages() {
-        const count = 1;
+    function SearchImages(query) {
         const [state, setState] = useState([]);
         useEffect(() => {
             axios
-                .get("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=e74f279994089eba87d7030954569145&per_page=9&format=json&nojsoncallback=1")
+                .get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e74f279994089eba87d7030954569145&per_page=9&format=json&nojsoncallback=1&text="+query)
                 .then((data)=>{
                     setState(data.data.photos.photo)
                 })    
-        }, [count])
+        }, [query])
         
         return (
                 state
@@ -24,11 +22,11 @@ const Api = () => {
         return `https://live.staticflickr.com/${server}/${id}_${secret}_q.jpg`;
     }
 
-    const data = GetImages();
-    
-    return(
+    const data = SearchImages();
+
+
+    return (
         <div>
-            <div>
             {data.map((img, key)=>{
                 const { farm, server, id, secret } = img;
                 return(
@@ -36,14 +34,8 @@ const Api = () => {
                 )
                 
             })}
-            </div>
         </div>
     )
-
-
-
-    
-    
 }
 
-export default Api;
+export default Search
